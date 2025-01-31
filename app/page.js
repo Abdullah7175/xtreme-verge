6e391eb38c0451c8f840e7654428e21051dc12f6
Abdullah7175 <abdullah7175@gmail.com>
2025-01-31 15:45:54 +0500

    commit

--- app/page.js
+++ app/page.js
@@ -106,8 +106,13 @@ export default function Home() {
 
               <div className="mt-6 space-x-4 action-buttons-wrapper">
                 <div className="action-buttons flex gap-4">
-                  <button className="relative bg-gray-400 rounded-lg px-2 py-3 before:absolute before:top-0 before:bottom-0 before:hover:bg-green-500">
-                    Get Started
+                <button
+                    className="relative px-3 py-2 text-black border rounded-full border-blue-200 font-base hover:-translate-y-1 transition-all duration-300 ease-in-out 
+                    hover:shadow-[inset_-3.6em_0_0_0_theme(colors.blue.700),inset_3.5em_0_0_0_theme(colors.blue.700)] 
+                    focus:shadow-[inset_-3.6em_0_0_0_theme(colors.red.700),inset_3.5em_0_0_0_theme(colors.red.700)] 
+                   hover:text-white"
+                  >
+                    Get started
                   </button>
 
                   <button
